bb43607a48a8c49f19bc2d90495885f09f6bcc2c
bintech <phamtan15991@gmail.com>
2016-08-26 02:48:54 +0700

    update MotorController.h

--- Buzzer.cpp
+++ Buzzer.cpp
@@ -5,6 +5,20 @@
  *      Author: tanpt
  */
 
+#include "Buzzer.h"
 
+static DigitalOut  Buzzer::buzzer_(LED1, 1);
 
+Buzzer::Buzzer(){
+}
 
+Buzzer::~Buzzer(){
+}
+
+void Buzzer::turnOn(){
+	buzzer_ = 1;
+}
+
+void Buzzer::turnOff(){
+	buzzer_ = 0;
+}
--- Buzzer.h
+++ Buzzer.h
@@ -8,8 +8,21 @@
 #ifndef BUZZER_H_
 #define BUZZER_H_
 
+#include "common.h"
 
+class Buzzer {
 
+public:
+	Buzzer();
+	~Buzzer();
+
+	void turnOn();
+
+	void turnOff();
+
+private:
+	static DigitalOut  buzzer_;
+};
 
 
 #endif /* BUZZER_H_ */
